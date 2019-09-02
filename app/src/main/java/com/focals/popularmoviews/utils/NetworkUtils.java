c4fa8dae74789a06620c11889dbbd8185d93e9d3
Abhi Handa <ahanda@ebay.com>
2019-09-02 15:49:33 -0700

    Adding a Network Utility class.

---
+++ app/src/main/java/com/focals/popularmoviews/utils/NetworkUtils.java
@@ -0,0 +1,64 @@
+package com.focals.popularmoviews.utils;
+
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class NetworkUtils {
+
+    private final static String API_KEY = "900e5f3653a851c0593341c8edb05ad6";
+    private final static String POPULAR_MOVIES_BASE_URL = "https://api.themoviedb.org/3/movie/popular";
+
+    public static URL buildUrl(String baseUrl) {
+        URL url = null;
+
+        Uri uri = Uri.parse(baseUrl).buildUpon()
+                .appendQueryParameter("api_key", API_KEY)
+                .appendQueryParameter("language", "en-US")
+                .build();
+
+        try {
+            url = new URL(uri.toString());
+
+            System.out.println();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    public static String getResponseFromUrl(URL url) {
+        HttpURLConnection httpURLConnection = null;
+
+        try {
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+
+            InputStream in = httpURLConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            if (scanner.hasNext()) {
+                return scanner.next();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            httpURLConnection.disconnect();
+        }
+
+        return null;
+    }
+
+    public static URL getPopularMoviesURL() {
+        return buildUrl(POPULAR_MOVIES_BASE_URL);
+    }
+
+}
