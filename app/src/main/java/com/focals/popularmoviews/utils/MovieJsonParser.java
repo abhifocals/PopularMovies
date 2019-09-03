910379ddf36d71bb931d4b56917216d82a02250e
Abhi Handa <ahanda@ebay.com>
2019-09-03 09:24:13 -0700

    Adding a parsing utility.

---
+++ app/src/main/java/com/focals/popularmoviews/utils/MovieJsonParser.java
@@ -0,0 +1,49 @@
+package com.focals.popularmoviews.utils;
+
+import com.focals.popularmoviews.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MovieJsonParser {
+
+    Movie movie;
+
+
+    public static void buildMovieArray(String response) {
+
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+
+            JSONArray results = jsonObject.getJSONArray("results");
+
+            System.out.println();
+
+
+
+            // Iterate through response and build Array of MovieObjects
+
+
+
+
+
+
+
+
+
+
+
+
+            jsonObject.getJSONArray("results").getJSONObject(0).getString("title");
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+}
