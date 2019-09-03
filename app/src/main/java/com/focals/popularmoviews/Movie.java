70ea7f423fa930b98ba3f3f167095863dc65b16b
Abhi Handa <ahanda@ebay.com>
2019-09-03 09:24:32 -0700

    Adding a Movie class to make Movie objects.

---
+++ app/src/main/java/com/focals/popularmoviews/Movie.java
@@ -0,0 +1,12 @@
+package com.focals.popularmoviews;
+
+public class Movie {
+
+    String title;
+    String poster_path;
+
+    Movie(String title, String poster_path) {
+        this.title = title;
+        this.poster_path = poster_path;
+    }
+}
